4b614390ffdfb7c0950661eb023ee801be8aa7d3
migiyubi <marugotoniboshi@gmail.com>
2026-04-14 22:18:34 +0900

    Remove obsolete class

--- src/js/object/Color.js
+++ src/js/object/Color.js
@@ -38,55 +38,6 @@ export class Color {
     }
 }
 
-class Color_ {
-    constructor(displayName, threeCode, hexString, rgbString) {
-        this._displayName = displayName;
-        this._threeCode = threeCode;
-        this._hexString = hexString;
-        this._rgbstring = rgbString;
-    }
-
-    get displayName() { return this._displayName; }
-    get threeCode() { return this._threeCode; }
-    get hexString() { return this._hexString; }
-    get rgbString() { return this._rgbstring; }
-
-    static get White() { return 1; }
-    static get Black() { return 2; }
-    static get Blue() { return 3; }
-    static get Yellow() { return 4; }
-
-    static _instanceMap = null;
-
-    static get map() {
-        if (this._instanceMap === null) {
-            this._instanceMap = {};
-
-            for (const data of master) {
-                const r = data.threeCode >> 16 & 0xff;
-                const g = data.threeCode >> 8 & 0xff;
-                const b = data.threeCode & 0xff;
-
-                const hexString = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
-                const rgbString = `rgb(${r},${g},${b})`;
-
-                this._instanceMap[data.enumeration] = new Color(
-                    data.displayName,
-                    data.threeCode,
-                    hexString,
-                    rgbString
-                );
-            }
-        }
-
-        return this._instanceMap;
-    }
-
-    static get(colorCode) {
-        return Color.map[colorCode] !== undefined ? Color.map[colorCode] : null;
-    }
-}
-
 const master = [
     {
         enumeration: Color.White,
